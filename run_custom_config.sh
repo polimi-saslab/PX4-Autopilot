d433fd25eac31a557e0d4036289450e836aaf129
Yanniscod <yannis.coderey@epfl.ch>
2025-04-02 14:44:34 +0200

    run_config.sh script + adding lib path in px4-auto process to no longer have to manually set it

---
+++ run_custom_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script ensures you're all set to run the PX4-Autopilot with tethered drone
+############## WIP ##############
+
+# Set plugin lib path for force plugin
+export GZ_SIM_SYSTEM_PLUGIN_PATH=~/tether_control_ws/install/tether_control/lib
