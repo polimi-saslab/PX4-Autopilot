a9291af5c80964d3bbdc792170dd378bb6ee561a
Yanniscod <yannis.coderey@epfl.ch>
2025-08-25 15:51:36 +0200

    rm script

--- run_custom_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script ensures you're all set to run the PX4-Autopilot with tethered drone
-############## WIP ##############
-
-# Set plugin lib path for force plugin
-echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=~/tether_control_ws/install/tether_control/lib/' >> ~/.bashrc
